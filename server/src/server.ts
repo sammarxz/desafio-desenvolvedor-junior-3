7e6b04751072fea91c24fd8f66c2d782e0c82ce7
Sam Marxz <sammarxzz@gmail.com>
2023-09-26 13:27:28 -0300

    feat: add swagger with openApi setting

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,51 @@
-import fastify from 'fastify';
+import Fastify, { FastifyInstance } from 'fastify';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
-const server = fastify();
+const swaggerOptions = {
+  openapi: {
+    info: {
+      title: 'Fastify API',
+      description: 'API for Softmakers developer test',
+      version: '1.0.0',
+    },
+  },
+};
 
-server.get('/ping', async () => {
-  return 'pong\n';
-});
+const swaggerUiOptions = {
+  routePrefix: '/docs',
+  exposeRoute: true,
+  staticCSP: true,
+};
 
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
+export class Server {
+  private server: FastifyInstance;
+
+  constructor() {
+    this.server = Fastify();
+
+    this.configurePlugins();
+    this.configureRoutes();
   }
-  console.log(`Server listening at ${address}`);
-});
+
+  private async configurePlugins() {
+    this.server.register(fastifySwagger, swaggerOptions);
+    this.server.register(fastifySwaggerUi, swaggerUiOptions);
+  }
+
+  private async configureRoutes() {
+    this.server.get('/healthcheck', async function () {
+      return { status: 'OK' };
+    });
+  }
+
+  async start({ port = 3000 }: { port?: number }) {
+    try {
+      await this.server.listen({ port });
+      console.log(`Server ready at http://localhost:${port}`);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
+  }
+}
