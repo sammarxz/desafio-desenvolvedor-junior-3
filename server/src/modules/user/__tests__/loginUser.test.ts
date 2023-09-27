6ebd4030ba00ab9a093a2317001bacdd574f5a85
Sam Marxz <sammarxzz@gmail.com>
2023-09-27 01:00:44 -0300

    test: loginUser

---
+++ server/src/modules/user/__tests__/loginUser.test.ts
@@ -0,0 +1,95 @@
+import Fastify, {
+  FastifyBaseLogger,
+  FastifyInstance,
+  FastifyTypeProviderDefault,
+  RawServerDefault,
+} from 'fastify';
+import { faker } from '@faker-js/faker';
+import t, { test } from 'tap';
+import { IncomingMessage, ServerResponse } from 'http';
+import { UserType } from '@fastify/jwt';
+
+import { initializeServer } from '../../../initializeServer';
+import { prisma } from '../../../lib/prisma';
+
+let server: FastifyInstance<
+  RawServerDefault,
+  IncomingMessage,
+  ServerResponse<IncomingMessage>,
+  FastifyBaseLogger,
+  FastifyTypeProviderDefault
+>;
+
+test('POST `/api/users/login`', async () => {
+  t.before(() => {
+    server = Fastify();
+    initializeServer(server);
+  });
+
+  test('given the correct email and password', async (t) => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+
+    t.teardown(async () => {
+      await prisma.user.deleteMany({});
+    });
+
+    await server.inject({
+      method: 'POST',
+      url: 'api/users',
+      payload: {
+        email,
+        password,
+      },
+    });
+
+    const response = await server.inject({
+      method: 'POST',
+      url: '/api/users/login',
+      payload: {
+        email,
+        password,
+      },
+    });
+
+    t.equal(response.statusCode, 200);
+
+    const verified = server.jwt.verify<UserType>(response.json().accessToken);
+
+    t.equal(verified.email, email);
+    t.type(verified.id, 'string');
+  });
+
+  test('given the incorrect email and password', async (t) => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+
+    t.teardown(async () => {
+      await prisma.user.deleteMany({});
+    });
+
+    await server.inject({
+      method: 'POST',
+      url: '/api/users',
+      payload: {
+        email,
+        password,
+      },
+    });
+
+    const response = await server.inject({
+      method: 'POST',
+      url: '/api/users/login',
+      payload: {
+        email,
+        password: 'wrong',
+      },
+    });
+
+    t.equal(response.statusCode, 401);
+
+    const json = response.json();
+
+    t.equal(json.message, 'Invalid email or password');
+  });
+});
